cf29d31b59e67c375310a0445808b9227b6bfe5d
José Dall' Asta <nekitbr3131@gmail.com>
2023-06-01 13:03:18 -0300

    lint

--- src/components/Map/MarkerList.tsx
+++ src/components/Map/MarkerList.tsx
@@ -3,7 +3,6 @@ import Icon from '../layout/Icon'
 import { Collapse, IconButton, List, ListItem, ListItemPrefix, ListItemSuffix, Typography } from '../material-tailwind/components'
 
 import type { iCustomGoogleMap } from './CustomGoogleMap'
-import { useEffect, useState } from 'react'
 
 interface MarkerListProps {
   items: iCustomGoogleMap.CustomMarker[]
@@ -16,54 +15,45 @@ interface MarkerListProps {
 export default function MarkerList({ selectedMarker, onClickItem, selection, items, onDelete }: MarkerListProps = { items: [] }) {
   return (
     <div>
-      <Typography
-        sx={{ mt: 4, mb: 2 }}
-        variant="h6"
-        component="div"
-      >
-        Your Markers
-      </Typography>
-      <div>
-        <Collapse open>
-          <List className="bg-white border rounded">
-            {items?.map((item, index, arr) => (
-              <>
-                <ListItem
-                  key={index}
-                  selected={selectedMarker?.position.lat === item?.position.lat && selectedMarker?.position.lng === item?.position.lng}
-                  onClick={() => {
-                    if (onClickItem) onClickItem(item)
-                  }}
-                  className="hover:bg-slate-300"
-                >
-                  <ListItemPrefix>
-                    <Icon iconName={item?.icon ?? 'location_on'} />
-                  </ListItemPrefix>
+      <Collapse open>
+        <List className="bg-white border rounded">
+          {items?.map((item, index, arr) => (
+            <>
+              <ListItem
+                key={index}
+                className="hover:bg-blue-gray-50/40"
+                selected={selectedMarker?.position.lat === item?.position.lat && selectedMarker?.position.lng === item?.position.lng}
+                onClick={() => {
+                  if (onClickItem) onClickItem(item)
+                }}
+              >
+                <ListItemPrefix>
+                  <Icon iconName={item?.icon ?? 'location_on'} />
+                </ListItemPrefix>
 
-                  {/* TODO: double click libera edição de texto, enter salva o texto, emit pro elemento, update no mapa. */}
-                  {item.label}
+                {/* TODO: double click libera edição de texto, enter salva o texto, emit pro elemento, update no mapa. */}
+                {item.label}
 
-                  {onDelete ? (
-                    <ListItemSuffix>
-                      <IconButton
-                        onClick={(event) => {
-                          event.stopPropagation()
-                          onDelete(item)
-                        }}
-                        variant="text"
-                        color="blue-gray"
-                      >
-                        <Icon iconName="delete" />
-                      </IconButton>
-                    </ListItemSuffix>
-                  ) : null}
-                </ListItem>
-                {index + 1 < arr.length && <Divider />}
-              </>
-            ))}
-          </List>
-        </Collapse>
-      </div>
+                {onDelete ? (
+                  <ListItemSuffix>
+                    <IconButton
+                      onClick={(event) => {
+                        event.stopPropagation()
+                        onDelete(item)
+                      }}
+                      variant="text"
+                      color="blue-gray"
+                    >
+                      <Icon iconName="delete" />
+                    </IconButton>
+                  </ListItemSuffix>
+                ) : null}
+              </ListItem>
+              {index + 1 < arr.length && <Divider />}
+            </>
+          ))}
+        </List>
+      </Collapse>
     </div>
   )
 }
