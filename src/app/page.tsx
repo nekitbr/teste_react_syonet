5d8eff62273a02ee39fd0260e12c23dfd6f1021c
José Dall' Asta <nekitbr3131@gmail.com>
2023-06-01 04:57:30 -0300

    construct main homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,91 @@
-import Image from 'next/image'
+'use client'
+
+import type { iCustomGoogleMap } from '@/components/Map/CustomGoogleMap'
+import CustomGoogleMap from '@/components/Map/CustomGoogleMap'
+import LocationSelect from '@/components/Map/LocationSelect'
+import MarkerList from '@/components/Map/MarkerList'
+import Icon from '@/components/layout/Icon'
+import Row from '@/components/layout/Row'
+import { Collapse, IconButton, Typography } from '../components/material-tailwind/components'
+import { useLoadScript } from '@react-google-maps/api'
+import { useRef, useState } from 'react'
+
+// defining outside component to avoid useEffect re-render with loadScript
+const libraries: ['places'] = ['places']
 
 export default function Home() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries,
+  })
+
+  const [selectedMarker, setSelectedMarker] = useState<iCustomGoogleMap.CustomMarker>()
+  const mapRef = useRef<iCustomGoogleMap.ForwardedRef>()
+  const [markers, setMarkers] = useState<iCustomGoogleMap.CustomMarker[]>([])
+
+  function handleHighlightMarker(marker: iCustomGoogleMap.CustomMarker) {
+    setSelectedMarker(marker)
+    mapRef.current?.setSelectedMarker(marker)
+    mapRef.current?.mapRef.current?.panTo(marker.position)
+  }
+
+  function handleNewMarkers(
+    newMarker: iCustomGoogleMap.CustomMarker | null,
+    allMarkers: iCustomGoogleMap.CustomMarker[],
+    action: iCustomGoogleMap.NewMarkerAction
+  ) {
+    setMarkers(allMarkers)
+
+    if (action === 'delete' || !newMarker) return
+
+    mapRef.current?.setSelectedMarker(newMarker)
+    setMarkers(allMarkers)
+  }
+
+  function handleSaveMarker() {
+    if (!mapRef.current) return
+
+    const savedMarker = mapRef.current.getSelectedMarker()
+    if (savedMarker) {
+      mapRef.current?.persistMarker(savedMarker)
+      mapRef.current?.clearSelectedMarker()
+    }
+  }
+
+  if (isLoaded)
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    Get started by editing&nbsp;
-                    <code className="font-mono font-bold">src/app/page.tsx</code>
-                </p>
-                <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-                    <a
-                        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        By{' '}
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            className="dark:invert"
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
-                </div>
-            </div>
-
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-                <Image
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
-
-            <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={'mb-3 text-2xl font-semibold'}>
-                        Docs <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                    </h2>
-                    <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>Find in-depth information about Next.js features and API.</p>
-                </a>
-
-                <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={'mb-3 text-2xl font-semibold'}>
-                        Learn <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                    </h2>
-                    <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-                </a>
-
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={'mb-3 text-2xl font-semibold'}>
-                        Templates <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                    </h2>
-                    <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>Explore the Next.js 13 playground.</p>
-                </a>
-
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={'mb-3 text-2xl font-semibold'}>
-                        Deploy <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                    </h2>
-                    <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-                </a>
-            </div>
-        </main>
+      <div className="text-white bg-blue-gray-900 flex min-w-full h-full w-full min-h-full">
+        <div className="w-1/4 p-4 bg-blue-gray-800">
+          <h1>Add new markers:</h1>
+          <Row>
+            <LocationSelect onSelect={handleHighlightMarker} />
+            <Icon
+              button
+              onClick={handleSaveMarker}
+              variant="filled"
+              iconName="add"
+              size="md"
+              color="blue-gray"
+            />
+          </Row>
+          <div>
+            <Collapse open={markers.length > 0}>
+              <MarkerList
+                selection
+                selectedMarker={selectedMarker}
+                onClickItem={handleHighlightMarker}
+                onDelete={(a) => mapRef.current?.removeMarker(a)}
+                items={markers}
+              />
+            </Collapse>
+          </div>
+        </div>
+        <div className="w-3/4 min-h-full">
+          <CustomGoogleMap
+            onNewMarker={handleNewMarkers}
+            ref={mapRef}
+          />
+        </div>
+      </div>
     )
 }
